dc4bd1d4a470418c12e4389a4a3d2b5a4c00cad0
mossaddak <mossaddak15-2413@diu.edu.bd>
2023-05-01 18:31:39 +0600

    updated

--- core/settings.py
+++ core/settings.py
@@ -215,8 +215,8 @@ TIME_ZONE = 'Asia/Dhaka'
 
 
 
-import os
-STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# import os
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
